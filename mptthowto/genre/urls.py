8ab3fd00f960227210b67d72e45e7366e8b9b6cc
Andrey Kaygorodov <info@kaygorodov.com>
2012-07-17 16:36:39 +0800

    added "delete action"

--- mptthowto/genre/urls.py
+++ mptthowto/genre/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls.defaults import patterns, include, url
-from django.views.generic import DetailView, ListView, CreateView, UpdateView
+from django.views.generic import DetailView, ListView, CreateView, UpdateView, DeleteView
 from genre.models import Genre
 
 urlpatterns = patterns('',
     url(r'detail/(?P<pk>\d+)', DetailView.as_view(model=Genre), name="genre_detail",),
     url(r'update/(?P<pk>\d+)', UpdateView.as_view(model=Genre), name="genre_update",),
+    url(r'delete/(?P<pk>\d+)', DeleteView.as_view(model=Genre, success_url='list'), name="genre_delete",),
     url(r'create', CreateView.as_view(model=Genre), name="genre_create",),
     url(r'list', ListView.as_view(model=Genre), name="genre_list",),
 )
